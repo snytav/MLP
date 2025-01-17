60c8f4dce59b39b67ee4bd01e22b88b3c922d2e5
snytav <snytav@gmail.com>
2025-01-17 17:54:48 +0200

    цршду екфштштп дщщз

--- main.py
+++ main.py
@@ -14,27 +14,28 @@ def predict(net, x, y, title):
     plt.plot(x.numpy(), y_pred.data.numpy(), 'o',
              c='r', label='Prediction')
     plt.title(title)
-    plt.show(block=True)
+    #plt.show(block=True)
 
 x_train = torch.rand(100)
 x_train = x_train*20 - 10.0
 y_train = torch.sin(x_train)
 plt.plot(x_train.numpy(),y_train.numpy(),'o')
-plt.show(block=True)
+#plt.show(block=True)
 noise = torch.randn(y_train.shape)/5.0
 plt.plot(x_train.numpy(),noise.numpy(),'o')
 plt.title('Gaussian noise')
-plt.show(block=True)
+# plt.show(block=True)
 y_train = y_train+noise
 plt.plot(x_train.numpy(),y_train.numpy(),'o')
 plt.title('noisy sine')
-plt.show(block=True)
+#plt.show(block=True)
 x_train.unsqueeze_(1)
 y_train.unsqueeze_(1)
 
 x_train, x_validation, y_train, y_validation = train_test_split(
     x_train, y_train, test_size=0.2, random_state=42)
 
+x_validation
 # x_validation = torch.linspace(-10,10,100)
 # y_validation = torch.sin(x_validation)
 plt.figure(4)
@@ -42,10 +43,10 @@ plt.plot(x_validation.numpy(),y_validation.numpy(),'o')
 plt.title('sin(x)')
 plt.xlabel('x_validation')
 plt.ylabel('y_validation')
-x_validation.unsqueeze_(1)
-y_validation.unsqueeze_(1)
+# x_validation.unsqueeze_(1)
+# y_validation.unsqueeze_(1)
 plt.title('validation set')
-plt.show(block=True)
+#plt.show(block=True)
 class SineNet(torch.nn.Module):
      def __init__(self,n_hidden_neurons):
         super(SineNet,self).__init__()
@@ -63,20 +64,22 @@ print('sn created')
 
 plt.figure(5)
 predict(sn,x_validation,y_validation,'1st prediction attempt ')
-optimizer = torch.optim.Adam(sn.parameters(),lr=0.01)
+optimizer = torch.optim.Adam(sn.parameters(),lr=0.1)
 
 ##### Training procedure
 loss_val = torch.ones(1)*10
-for epoch_index in range(500):
+epoch_index = 0
+while loss_val.item() > 1e-2:
      print('Epoch===================== ',epoch_index,loss_val.item())
      optimizer.zero_grad()
      y_pred = sn.forward(x_train)
      loss_val = lossf(y_pred,y_train)
      loss_val.backward()
      optimizer.step()
+     epoch_index = epoch_index + 1
  #print(epoch_index)
-print('FINAL LOSS VALUE ',loss_val)
+print('FINAL LOSS VALUE ',loss_val.item())
 plt.figure(6)
 predict(sn,x_validation,y_validation,'prediction after training')
-plt.show(block=True)
+#plt.show(block=True)
 
